047ddd61bb595b143aa3f1d29aba8365bfd85c28
Dairo <parra.dairo@gmail.com>
2021-12-27 22:22:49 -0500

    feature: add new files proyect

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+Route::put('/posts/{post}', [PostController::class, 'update']);
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+
+Route::post('/posts', [PostController::class, 'store']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
